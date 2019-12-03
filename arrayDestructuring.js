d847e2442d8f8b20f45e2ae5e106ed822081b8c1
Sean-Kim84 <sean.kim843@gmail.com>
2019-12-03 16:10:42 +0900

    ES6 Array Destructuring

---
+++ arrayDestructuring.js
@@ -0,0 +1,9 @@
+const address = ['1299 Teheran', 'Korea', 'Kojec', '11023'];
+const [street, city, state, zip] = address;
+
+console.log(street, city, state, zip);
+
+const person = [];
+const [ living="Korea", age=10 ] = person;
+
+console.log(living, age);
