7874db746c2eae8d54f546279454cae69eb15410
Sean-Kim84 <sean.kim843@gmail.com>
2019-12-03 14:53:04 +0900

    Dispatching Action

--- redux101.js
+++ redux101.js
@@ -1,7 +1,55 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = {count = 0}) => {
+const store = createStore((state = { count: 0 }, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+    case 'DECREMENT':
+      return {
+        count: state.count -1
+      }
+    case 'RESET': 
+      return {
+        count: 0
+      }
+    default :
+      return state
+    }
+  // if(action.type === 'INCREMENT'){
+  //   return {
+  //     count: state.count + 1
+  //   };
+  // } else {
+  //   return state
+  // }
+  console.log('running');
   return state;
-});
+}); 
 
 console.log(store.getState());
+
+/* Action - than an object that gets sent to store */
+// I'd like to increment the count
+store.dispatch({
+  type: 'INCREMENT',
+});
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
+})
+
+console.log(store.getState()); // getState 로 state를 가져올 수있다.
+
+
+
+// I'd like to reset the count to zero
