742e2a58add9fb4d42337f49a5ab126e81484318
Sean-Kim84 <sean.kim843@gmail.com>
2019-12-03 18:44:03 +0900

    ES6 Spread Operator in Reducers

--- redux-expensify.js
+++ redux-expensify.js
@@ -1,6 +1,29 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 // Add Expense
+const addExpense = (
+  { 
+    description = '', 
+    note = '', 
+    amount = 0, 
+    createdAt = 0
+} = {}) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+})
+
 // Remove Expense
 // Edit Expense
 // Set text filter
@@ -14,8 +37,16 @@ const expenseReducerDefaultState = [];
 
 const expenseReducer = (state = expenseReducerDefaultState, action) => {
   switch(action.type){
-
-
+    case 'ADD_EXPENSE': 
+      // return state.concat(action.expense)
+      return [
+        ...state, 
+        action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+        return state.filter(({ id }) => {
+          return id !== action.id
+        })
 
     default: 
       return state;
@@ -35,16 +66,35 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
     return state;
   }
 }
-
-
 // Store creation
 
-const store = createStore(combineReducers({
-  expenses: expenseReducer,
-  filters: filterReducer
-}));
-console.log(store.getState());
+const store = createStore(
+  combineReducers({
+    expenses: expenseReducer,
+    filters: filterReducer
+  })
+);
 
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+
+
+const expenseOne = store.dispatch(addExpense({
+  description: 'Rent',
+  amount: 100
+}));
+
+const expenseTwo = store.dispatch(addExpense({
+  description: 'Oil',
+  amount: 300
+}))
+
+store.dispatch((removeExpense({id: expenseOne.expense.id})))
+
+console.log(expenseOne);
 const demoState = {
   expenses: [{
     id: 'seasdfsd',
