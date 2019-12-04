1d38fd779adf32030a38784bc2b65633ba6e6963
Sean-Kim84 <sean.kim843@gmail.com>
2019-12-04 11:53:57 +0900

    Sorting Redux Data

--- redux-expensify.js
+++ redux-expensify.js
@@ -149,6 +149,13 @@ const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate} ) => {
 
 
     return startDateMatch && endDateMatch && textDateMatch;
+  }).sort((a,b) => {
+    if(sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if(sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1;
+    }
+    
   });
 }
 
@@ -167,22 +174,25 @@ store.subscribe(() => {
 });
 
 
-const expenseOne = store.dispatch(addExpense({
-  description: 'Rent',
-  amount: 100,
-  createdAt: 1000
-}));
+// const expenseOne = store.dispatch(addExpense({
+//   description: 'Rent',
+//   amount: 100,
+//   createdAt: 1000
+// }));
 
-const expenseTwo = store.dispatch(addExpense({
-  description: 'Oil',
-  amount: 300,
-  createdAt: -1000
-})) 
+// const expenseTwo = store.dispatch(addExpense({
+//   description: 'Oil',
+//   amount: 300,
+//   createdAt: -1000
+// })) 
+
+store.dispatch((addExpense({ description: "Rent", amount: 100, createdAt: -21000 })));
+store.dispatch((addExpense({ description: "Coffee", amount: 300, createdAt: -1000 })))
 
 // store.dispatch((removeExpense({ id: expenseOne.expense.id })));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter('RENT'));
+// store.dispatch(setTextFilter('RENT'));
 
 // store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
